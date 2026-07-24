386f583e0d077b32235bb1644d919d528186ffbd
deksor <deksor@theretroweb.com>
2026-07-24 22:13:01 +0200

    refacto: minor tweak

--- src/flash/all_algorithms.c
+++ src/flash/all_algorithms.c
@@ -1,9 +1,22 @@
 #include "uniflash/all_algorithms.h"
 
 bool uf_flash_register_all_algorithms(uf_flash_algorithm_registry_t *registry) {
-    return uf_flash_register_generic_algorithms(registry) && uf_flash_register_intel_algorithms(registry) &&
-           uf_flash_register_sharp_algorithms(registry) && uf_flash_register_pmc_algorithms(registry) &&
-           uf_flash_register_winbond_algorithms(registry) && uf_flash_register_macronix_algorithms(registry) &&
-           uf_flash_register_atmel_algorithms(registry) && uf_flash_register_sst_algorithms(registry) &&
-           uf_flash_register_st_algorithms(registry);
+    static bool (*const register_functions[])(uf_flash_algorithm_registry_t *registry) = {
+        uf_flash_register_generic_algorithms,
+        uf_flash_register_intel_algorithms,
+        uf_flash_register_sharp_algorithms,
+        uf_flash_register_pmc_algorithms,
+        uf_flash_register_winbond_algorithms,
+        uf_flash_register_macronix_algorithms,
+        uf_flash_register_atmel_algorithms,
+        uf_flash_register_sst_algorithms,
+        uf_flash_register_st_algorithms,
+    };
+
+    for (size_t i = 0; i < sizeof(register_functions) / sizeof(register_functions[0]); ++i) {
+        if (!register_functions[i](registry)) {
+            return false;
+        }
+    }
+    return true;
 }
